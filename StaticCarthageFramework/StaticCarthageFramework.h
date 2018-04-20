6651f461d5c3f370a70bf48c78dbdba7486bfbe3
Artem Novichkov <artem.novichkov@rosberry.com>
2018-04-20 10:13:56 +0600

    Add framework

---
+++ StaticCarthageFramework/StaticCarthageFramework.h
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2018 Rosberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StaticCarthageFramework.
+FOUNDATION_EXPORT double StaticCarthageFrameworkVersionNumber;
+
+//! Project version string for StaticCarthageFramework.
+FOUNDATION_EXPORT const unsigned char StaticCarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StaticCarthageFramework/PublicHeader.h>
+
+
