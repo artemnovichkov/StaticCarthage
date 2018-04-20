f5f10e699dac6085d0fcc82abaab9e21af9b3b57
Artem Novichkov <artem.novichkov@rosberry.com>
2018-04-20 13:18:49 +0600

    Add xcassets

--- StaticCarthageFramework/MyClass.swift
+++ StaticCarthageFramework/MyClass.swift
@@ -2,11 +2,15 @@
 //  Copyright © 2018 Rosberry. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 public class MyClass {
 
     public static func log() {
         print("It works!")
+    }
+
+    public static func image() -> UIImage {
+        return #imageLiteral(resourceName: "chevron")
     }
 }
